a14ff7648c0379ea70f8844e17105e3d97268432
emilio <belmonteoilime@gmail.com>
2019-05-03 12:48:57 +0200

    Callback de la clase all y implementacion del action bar con error(Do not request Window.FEATURE_SUPPORT_ACTION_BAR and set windowActionBar to false in your theme to use a Toolbar instead)

--- app/src/main/java/com/example/apistarwar/api/InterfaceStarWarApi.kt
+++ app/src/main/java/com/example/apistarwar/api/InterfaceStarWarApi.kt
@@ -1,10 +1,11 @@
 package com.example.apistarwar.api
 
+import com.example.apistarwar.data.All
 import com.example.apistarwar.data.People
 import retrofit2.Call
 import retrofit2.http.GET
 
 interface InterfaceStarWarApi {
 
-    @GET("people") fun getPeople(): Call<People>
+    @GET("people") fun getPeople(): Call<All>
 }
