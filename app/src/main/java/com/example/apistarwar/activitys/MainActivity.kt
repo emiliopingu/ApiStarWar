38ab339e33d1fa5f683fed0884727d33893b5eb4
emilio <belmonteoilime@gmail.com>
2019-05-08 08:44:39 +0200

    Borrado del codigo

--- app/src/main/java/com/example/apistarwar/activitys/MainActivity.kt
+++ app/src/main/java/com/example/apistarwar/activitys/MainActivity.kt
@@ -2,74 +2,26 @@ package com.example.apistarwar.activitys
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.widget.LinearLayoutManager
-import android.util.Log
-import android.view.Menu
-import android.widget.Toast
 import com.example.apistarwar.R
-import com.example.apistarwar.adapter.PeopleListAdapter
-import com.example.apistarwar.api.RetrofitClient
-import com.example.apistarwar.data.All
-import com.example.apistarwar.data.People
-import kotlinx.android.synthetic.main.activity_main.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+
+
 
 class MainActivity : AppCompatActivity() {
 
-    val listPeople: MutableList<People> = ArrayList()
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setSupportActionBar(toolbar)
-        val actionBar = supportActionBar
-        actionBar!!.title = "Enciclopedia de STW"
-        actionBar.elevation = 5.0F
-        actionBar.setDisplayShowHomeEnabled(true)
-        actionBar.setDisplayShowHomeEnabled(true)
-        actionBar.setDisplayUseLogoEnabled(true)
-        CallApi()
+
     }
 
-fun inflater(){
-        val layoutManager = LinearLayoutManager(this@MainActivity)
-        layoutManager.orientation = LinearLayoutManager.VERTICAL
-        recyclerView.layoutManager = layoutManager
-        val adapter =PeopleListAdapter(this@MainActivity, listPeople)
-        recyclerView.adapter = adapter
 
-}
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        val inflater = menuInflater
-        inflater.inflate(R.menu.menu, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
 
-    fun CallApi() {
-        RetrofitClient.service.getPeople().enqueue(object : Callback<All> {
-            override fun onResponse(call: Call<All>, response: Response<All>) {
 
-                if (response.isSuccessful) {
-                    val i: Int = response.body()!!.people!!.size
-                    for (character in 0 until i) {
-                        listPeople.add(response.body()!!.people!![character])
-                        Log.i("tag1", "Se ha añadidio")
-                    }
-                    inflater()
-                }
-            }
 
-            override fun onFailure(call: Call<All>, t: Throwable) {
-                Log.i("falloCallback", "Ha fallado el Callback<All")
-                Toast.makeText(this@MainActivity, "No se pudo conectar con la API", Toast.LENGTH_LONG)
-            }
-
-        })
-    }
 
 }
 
